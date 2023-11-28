535e1c8fe32b40c5fade9d89d7aac497519e9df5
apalkowski <aleksander.palkowski@gmail.com>
2023-11-28 12:15:05 +0100

    Update netmhc2pan

--- vaxcollapse/netmhc2pan.py
+++ vaxcollapse/netmhc2pan.py
@@ -8,3 +8,15 @@ Default threshold for weak binders (%Rank): 5.0
 
 Copyright 2023 Aleksander Pałkowski.
 """
+
+import pandas as pd
+
+
+def read_netmhc2pan_results(results_file: str) -> tuple[pd.DataFrame, str]:
+    """Read NetMHCIIpan results file and return it as DataFrame and allele annotation."""
+    with open(results_file, "r") as handle:
+        allele = handle.readline().strip()
+
+    results = pd.read_csv(results_file, sep="\t", skiprows=1)
+
+    return results, allele
